ce01f5a3d252927efa69d8a437c4eb1118fb770a
Akam Programist <akadev404@gmail.com>
2023-02-21 23:44:06 +0300

    commits

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,65 +2,93 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\RedirectResponse;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class TasksController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(): Response
-    {
-        //
-    }
+    use HttpResponses;
 
     /**
-     * Show the form for creating a new resource.
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
-    public function create(): Response
+    public function index()
     {
-        //
+        return TaskResource::collection(
+            Task::where('user_id', Auth::user()->id)->get()
+        );
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
-    public function store(Request $request): RedirectResponse
+    public function store(StoreTaskRequest $request)
     {
-        //
+        $request->validated();
+
+        $task = Task::create([
+            'user_id' => Auth::user()->id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'priority' => $request->priority
+        ]);
+
+        return new TaskResource($task);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function show(string $id): Response
+    public function show(Task $task)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id): Response
-    {
-        //
+        return $this->isNotAuthorized($task) ? $this->isNotAuthorized($task) : new TaskResource($task);
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, string $id): RedirectResponse
+    public function update(Request $request, Task $task)
     {
-        //
+        if (Auth::user()->id !== $task->user_id) {
+            return $this->error('', 'You are not authorized to make this request', 403);
+        }
+
+        $task->update($request->all());
+
+        return new TaskResource($task);
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(string $id): RedirectResponse
+    public function destroy(Task $task)
     {
-        //
+        return $this->isNotAuthorized($task) ? $this->isNotAuthorized($task) : $task->delete();
+    }
+
+    private function isNotAuthorized($task)
+    {
+        if (Auth::user()->id !== $task->user_id) {
+            return $this->error('', 'You are not authorized to make this request', 403);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+
+// public routes
+
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::resource('/tasks',TasksController::class);
+// protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('/tasks', TasksController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
